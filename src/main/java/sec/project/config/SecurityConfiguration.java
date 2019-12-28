a9472271b4dadc25fa96fb1289800a75edbebc50
dalibor-necas <58915489+dalibor-necas@users.noreply.github.com>
2019-12-28 19:27:16 +0100

    Update SecurityConfiguration.java

--- src/main/java/sec/project/config/SecurityConfiguration.java
+++ src/main/java/sec/project/config/SecurityConfiguration.java
@@ -20,6 +20,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable(); //Disable CSRF protection
         // no real security at the moment
         http.authorizeRequests()
                 .anyRequest().permitAll();
